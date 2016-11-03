c8defe2e1cfc046184a4d8115e6e30ec333a2919
Oliver <oliverloye@me.com>
2016-11-03 16:08:21 +0100

    Opdatering fra torsdag d. 3/11

--- Heroic Dungeon/src/dungeon/Dungeon.java
+++ Heroic Dungeon/src/dungeon/Dungeon.java
@@ -5,18 +5,6 @@ import java.util.Scanner;
 
 public class Dungeon
 {
-
-    /*
-    //Weapons
-    PulseGrenade pulseGrenade = new PulseGrenade(50, 100);
-    VibroBlade vibroBlade = new VibroBlade(20, 100);
-    LaserGun laserGun = new LaserGun(10, 100);
-    
-    //Health items
-    HippenMin smallHealingPot1 = new HippenMin(10);
-    HippenMid mediumHealingPot1 = new HippenMid(20);
-    HippenMax superHealingPot1 = new HippenMax(50);
-     */
     private Player player1;
     private Room current;
 
@@ -93,83 +81,66 @@ public class Dungeon
             if (answer.equalsIgnoreCase("help"))
             {
                 getHelp();
-            } else if (answer.equalsIgnoreCase("check cache"))
-            {
+            } else if (answer.equalsIgnoreCase("check cache")) {
                 System.out.println("\nTotal of Eridium: " + player1.getCache());
-            } else if (answer.equalsIgnoreCase("loot"))
-            {
-                if (current.getNumberOfEridium() > 0)
-                {
+            } else if (answer.equalsIgnoreCase("loot")) {
+                
+                if(current.getNumberOfEridium() == 0 && current.getRoomTreasure().isEmpty()) {
+                    System.out.println("There is nothing to loot!");
+                } else if(!(current.getRoomTreasure().isEmpty())) {
+                        ArrayList<Item> allItems = current.getRoomTreasure();
+                        player1.addAllToBag(allItems); //Lægger alle items fra arraylisten over i spilleres bag
+                        System.out.println("You've just looted " + current.getRoomTreasure().toString());
+                } else if(current.getNumberOfEridium() > 0) {
                     //current.setNumberOfEridium(current.getNumberOfEridium()); //slettes???
                     System.out.println("You've just looted " + current.getNumberOfEridium() + " chunk(s) of Eridium!");
 
-                    ArrayList<Item> allItems = current.getAllItems();
-                    player1.addAllToBag(allItems); //Lægger alle items fra arraylisten over i spilleres bag
-
                     player1.addToCache(current.getNumberOfEridium());
                     current.setNumberOfEridium(0);
-
-                    //System.out.println("You now have: " + player1.getCache() + " chunks of Eridium.");
-                } else
-                {
+                } else {
                     System.out.println("There is nothing to loot!");
                 }
-                if (current.getRoomNumber() == 20)
-                {
+                if (current.getRoomNumber() == 20) {
                     System.out.println("\nCongratulations! You reached the final room");
                     System.out.println("\nYou've collected a total of " + player1.getCache() + " Eridium");
                     System.exit(0);
                 }
-            } else if (answer.equalsIgnoreCase("hint"))
-            {
+            } else if (answer.equalsIgnoreCase("hint")) {
                 System.out.println(current.getHint());
-            } else if (answer.equalsIgnoreCase("wear"))
-            {
-                if (current.getRoomNumber() == 1)
-                {
+            } else if (answer.equalsIgnoreCase("wear")) {
+                if (current.getRoomNumber() == 1) {
                     player1.setWearSuit(true);
                     System.out.println("The suit is now equiped!");
-                } else
-                {
+                } else {
                     System.out.println("There is nothing to wear");
                 }
-            } else if (answer.equalsIgnoreCase("north"))
-            {
+            } else if (answer.equalsIgnoreCase("north")) {
                 goNorth();
                 checkChangingRoom();
-            } else if (answer.equalsIgnoreCase("east"))
-            {
+            } else if (answer.equalsIgnoreCase("east")) {
                 goEast();
                 checkChangingRoom();
-            } else if (answer.equalsIgnoreCase("south"))
-            {
+            } else if (answer.equalsIgnoreCase("south")) {
                 goSouth();
                 checkChangingRoom();
-            } else if (answer.equalsIgnoreCase("west"))
-            {
+            } else if (answer.equalsIgnoreCase("west")) {
                 goWest();
                 checkChangingRoom();
-            } else if (answer.equalsIgnoreCase("quit"))
-            {
+            } else if (answer.equalsIgnoreCase("quit")) {
                 play = false;
                 System.exit(0);
-            } else
-            {
+            } else if (answer.equalsIgnoreCase("bag")) {
+                System.out.println("Inventory: \n" + player1.getBag());
+            } else {
                 System.out.println("That is not a valid command!\nPlease try again.");
             }
         }
     }
 
-//    public void pickUpAll(){
-//        
-//        
-//        player1
-//    }
     public void createDungeon()
     {
 //Opret items og lægi rum:
-        VibroBlade v1 = new VibroBlade(12, 4);
-
+        
         //Opret monster         
 //Opretter objekter:
         Room loadingBay = new Room(1, 0, ""
@@ -179,51 +150,57 @@ public class Dungeon
                 + "\nIf you get freaked out, or bored, you can always Quit while you're ahead and flee the mines,"
                 + "\nbut in the hurry you will drop your Cache of Eridium. In this hangar there is absolutely nothing "
                 + "\nto Loot so you decide to press on. There is only one door in the vast hollowed out loading bay, and "
-                + "\nit is found to the North. You must now decide what to do next!", "Hint: Try moving into the next room.", 0, null);
+                + "\nit is found to the North. You must now decide what to do next!", "Hint: Try moving into the next room.", 0);
+        loadingBay.add(new HlpenMin());
         Room changingRoom = new Room(4, 1, "\nThe light flickers are on, you are standing in a changing room surrounded by what "
                 + "\nyou can best describe as hazmat suits, they are big and white and look very heavy, but manageable to carry "
                 + "\nand to wear. This helmet is way bigger than your head and it has a large square window in the front for "
                 + "\nlooking purposes equipped with a lamp to light up the dark caverns on the side. From where you stand, you "
                 + "\ncan now see four doors including the door back and three doors each with a sign over it. To the West is a "
                 + "\ndoor labelled “Excavated”, the Northernmost door is labelled “Main Shaft” while the last door to the East is "
-                + "\nlabelled “Dormitory”. What will you do?", "Mabye it's a good idea to \"wear\" that suit..?", 0, null);
+                + "\nlabelled “Dormitory”. What will you do?", "Mabye it's a good idea to \"wear\" that suit..?", 0);
         Room dumbLoud1 = new Room(2, 2, "\nYou've entered a long hallway, with rocky sides, and stallecmites shooting up from the ground everywhere. "
-                + "\nWhat will you do?", "Hint: Try moving on", 0, null);
+                + "\nWhat will you do?", "Hint: Try moving on", 0);
         Room dumbLoud2 = new Room(2, 3, "\nYou continue into the dark of the mines, taking a hard right, still, this barren cave-system, gives you the creeps"
-                + "\nWhat will you do?", "Hint: Try moving on", 2, null);
+                + "\nWhat will you do?", "Hint: Try moving on", 2);
         Room dumbLoud3 = new Room(2, 4, "\nYou move through the caves only being able to see the cone of light that forms where you look. A sudden shadow moves"
                 + "\nin your line of vision and you get startled, standing completely still, but darting your head around to catch another glimpse of the creature. "
                 + "\nNothing shows up, you stand there, paranoid, thinking that it might show up again, but, nevertheless, you can't hear, see or feel anything in this"
-                + "\ncave section. What will you do?", "Hint: Try moving on", 5, null);
-        Room dumbLoud4 = new Room(2, 5, "\nA litte further down the tunnel, is a faint light, but what is casting it?", "Hint: What could that light be?", 0, null);
-        Room dumbLoud5 = new Room(1, 6, "\nA dead end, but a dead end, filled with small Eridium chunks!", "Hint: Well, pick them up!", 8, null);
-        Room ingenuitySmart1 = new Room(3, 7, "You enter the white walled halls of the dormitory, what might await you within...", "Hint: Try moving on.", 0, null);
+                + "\ncave section. What will you do?", "Hint: Try moving on", 5);
+        Room dumbLoud4 = new Room(2, 5, "\nA litte further down the tunnel, is a faint light, but what is casting it?", "Hint: What could that light be?", 0);
+        Room dumbLoud5 = new Room(1, 6, "\nA dead end, but a dead end, filled with small Eridium chunks!", "Hint: Well, pick them up!", 8);
+        dumbLoud5.add(new VibroBlade());
+        Room ingenuitySmart1 = new Room(3, 7, "You enter the white walled halls of the dormitory, what might await you within...", "Hint: Try moving on.", 0);
         Room ingenuitySmart2 = new Room(2, 8, "\nYou enter a brightly lit room, the white walls are holey with large rocks protruding through. In the middle"
                 + "\nof the room there is an undamaged data terminal and the keyboard is written with one of MoriMoriTek's many corporate trademarked fonts—"
                 + "\nalmost unreadable. There is but one button in a sea of white which appears red. What will you do?",
-                "Hint: Feature not implemented yet, sorry.", 3, null);
-        Room ingenuitySmart3 = new Room(2, 9, "An empty room, with a couple of tables thrown about, nothing really noteworthy.", "Hint: Try moving on", 0, null);
+                "Hint: Feature not implemented yet, sorry.", 3);
+        Room ingenuitySmart3 = new Room(2, 9, "An empty room, with a couple of tables thrown about, nothing really noteworthy.", "Hint: Try moving on", 0);
+        ingenuitySmart3.add(new HlpenMid());
         Room ingenuitySmart4 = new Room(3, 10, "\nSomething catches your eyes! It was a mirror, you look at yourself, and review your face behinde the mask, "
-                + "\n'damn I look tired” you mumble in the mask.'", "Try moving on", 3, null);
+                + "\n'damn I look tired” you mumble in the mask.'", "Try moving on", 3);
         Room ingenuitySmart5 = new Room(2, 11, "A light flickers, but doesn't completely turn on. There is a sign just beneath the lamp reading Sector G. "
-                + "Does this remind you of something? What will you do?", "Hint: Feature not implemened yet, sorry.", 0, null);
+                + "Does this remind you of something? What will you do?", "Hint: Feature not implemened yet, sorry.", 0);
         Room ingenuitySmart6 = new Room(1, 12, "\nThis room is a smugglers dream with lots of hidden away goods. It sparkles in your eyes, "
                 + "\nrefined Eridium and RealChocolatetm Candy bars that even you thought was gone after being out of production for years now. "
                 + "\nUnderneath a pile of RealChocolatetm Candy bars, you find a small information card the size of ???, which you plug into your AiRM, "
                 + "\nand the AiRM reads it aloud to you: “Excerpt from the memoirs of Ian Stomache: We've been overrun, I've got no idea what hit us, but "
                 + "\nwe collided with something, something not of this universe. We tried to fight it, their weapons left no trace of our bodies like they "
                 + "\njust absorbed us. I better face this like a man and fight this on my own terms. I will leave this behind for anyone to find. I sure"
-                + "\nhope you fare better on this Fujinforsaken rock!”" + "What will you do?", "Hint: You aren't suppose to be here...", 25, null);
-        Room straightBland1 = new Room(2, 13, "You walk into the tunnel, it's steep, uphill clmibing.", "Hint: Try moving on", 1, v1);
-        Room straightBland2 = new Room(3, 14, "You see a man, walking around the corner, you chase want to chase after him?", "Hint: Chase the man, whom went left!", 0, null);
-        Room straightBland3 = new Room(2, 15, "You ignore the man and move straight, ", "Hint: You sure that man wasn't enticing? He wasn't wearing a suit...", 2, null);
-        Room straightBland4 = new Room(2, 16, "You get around the corner, but nothing is there...", "Hint: Look for the man", 0, null);
-        Room straightBland5 = new Room(3, 17, "You spot the man again, he is standing straight, staring at you", "Hint: Is he even human? Am I going crazy?", 1, null);
-        Room straightBland6 = new Room(2, 18, "You move towards him, he.. flickers? you now realize he was a Hologram, all along.", "Hint: Such a dissapointment", 3, null);
-        Room straightBland7 = new Room(2, 19, "You move further up a hillclimb inside the cavern, noticing a glow, at the end of the tunnel", "Hint: Is this what i think it is?", 0, null);
+                + "\nhope you fare better on this Fujinforsaken rock!”" + "What will you do?", "Hint: You aren't suppose to be here...", 25);
+        ingenuitySmart6.add(new PulseGrenade());
+        ingenuitySmart6.add(new PulseGrenade());
+        ingenuitySmart6.add(new PulseGrenade());
+        Room straightBland1 = new Room(2, 13, "You walk into the tunnel, it's steep, uphill clmibing.", "Hint: Try moving on", 1);
+        Room straightBland2 = new Room(3, 14, "You see a man, walking around the corner, you chase want to chase after him?", "Hint: Chase the man, whom went left!", 0);
+        Room straightBland3 = new Room(2, 15, "You ignore the man and move straight, ", "Hint: You sure that man wasn't enticing? He wasn't wearing a suit...", 2);
+        Room straightBland4 = new Room(2, 16, "You get around the corner, but nothing is there...", "Hint: Look for the man", 0);
+        Room straightBland5 = new Room(3, 17, "You spot the man again, he is standing straight, staring at you", "Hint: Is he even human? Am I going crazy?", 1);
+        Room straightBland6 = new Room(2, 18, "You move towards him, he.. flickers? you now realize he was a Hologram, all along.", "Hint: Such a dissapointment", 3);
+        Room straightBland7 = new Room(2, 19, "You move further up a hillclimb inside the cavern, noticing a glow, at the end of the tunnel", "Hint: Is this what i think it is?", 0);
         Room straightBland8 = new Room(1, 20, "\nAfter having climbed these tunnels and having encountered countless mirages, you find yourself lost "
                 + "\nand confused at the top of a chute. All around the chute lies countless small bundles of refined and unrefined Eridium ready "
-                + "\nfor being chucked down the hole. What will you do?", "Hint: Try picking it up!", 7, null);
+                + "\nfor being chucked down the hole. What will you do?", "Hint: Try picking it up!", 7);
 
         //Sætter alle referencer
         loadingBay.north = changingRoom;
@@ -383,11 +360,13 @@ public class Dungeon
     //Opretter spilleren
     public void createPlayer()
     {
-
+        
         System.out.println("\nWhat is your name?");
 
         player1 = new Player(100);
-
+        player1.addSingleToBag(new LaserGun());
+        player1.addSingleToBag(new HlpenMin());
+        player1.addSingleToBag(new HlpenMin());
         System.out.println("\nHi " + player1.getName() + "!");
 
     }

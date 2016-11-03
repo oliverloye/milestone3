d49af5090159f96bbf7d6730d565a18b81e797c3
Oliver <oliverloye@me.com>
2016-11-03 12:57:27 +0100

    Test comment i Combat class

--- Heroic Dungeon/src/dungeon/Combat.java
+++ Heroic Dungeon/src/dungeon/Combat.java
@@ -15,6 +15,8 @@ public class Combat {
     public void fightOneRound( Player p1, Monster monster   ){
         int dam = p1.getDamage();
         monster.applyWounds( dam   )
+                
+                // TEST GITBUH!!
         
     }
     
